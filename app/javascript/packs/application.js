f722b8c2ca559bf52e17047edc33ac1fb7adf087
harihari1009 <wellkazuua@gmail.com>
2019-12-03 09:32:51 +0000

    add image modules

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
+require.context('../images', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
