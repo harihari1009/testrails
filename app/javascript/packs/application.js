543dc051e9de37d23d433d05ad8da92676829124
harihari1009 <wellkazuua@gmail.com>
2019-12-07 19:23:34 +0000

    final tempalte for bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,15 @@
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
+import './lib/jquery/jquery'
+import './lib/bootstrap/js/bootstrap'
+import './lib/php-mail-form/validate'
+// import './lib/prettyphoto/js/prettyphoto'
+import './lib/hover/hoverdir'
+import './js/main'
 require.context('../images', true)
 
+console.log('test!');
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
